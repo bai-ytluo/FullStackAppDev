469ae4964ba125f7d4fa40b846c6af46fb61a9cd
bai-ytluo <ytluo@uchicago.edu>
2023-05-04 13:58:09 -0500

    Fixed bugs

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
-const express = require('express'); 
-const app = express(); 
-const routeTest = require('./routes/routeTest'); 
-const mongoose = require('mongoose'); 
+const express = require('express');
+const app = express();
+const routeTest = require('./routes/routeTest');
+const mongoose = require('mongoose');
 
-app.use('/api/v1/', routeTest); 
+app.use('/api/v1/', routeTest);
 
-mongoose.connect(
-    'mongodb+srv://adimin:admin@cluster0.nakegvh.mongodb.net/?retryWrites=true&w=majority', 
-    {
-        useNewUrlParser: true, 
-        useUnifiedTopology: true, 
-        dbName: 'testDB', 
-    }
-).then(() => {
-    console.log('Database Connection is Ready...'); 
-    app.listen(8080); 
-    console.log('Server Listening on Port 8080...'); 
-}).catch((err) => {
-    console.log(err); 
-}); 
+mongoose
+    .connect(
+        'mongodb+srv://adimin:admin@cluster0.nakegvh.mongodb.net/?retryWrites=true&w=majority',
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            dbName: 'testDB',
+        },
+    )
+    .then(() => {
+        console.log('Database Connection is Ready...');
+        app.listen(8080);
+        console.log('Server Listening on Port 8080...');
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 /* app.get("/users/:userId/books/:bookId", (req,res) => {
     res.send(req.params); 
--- routes/routeTest.js
+++ routes/routeTest.js
@@ -1,16 +1,19 @@
-const express = require('express'); 
-const router = express.Router(); 
-const UserTest = require('../models/userTest'); 
+const express = require('express');
+const router = express.Router();
+const UserTest = require('../models/userTest');
 //const authenticateService = require('../middleware/test')
 
-router.get('/meetfood', /* authenticateService,  */async (req,res)=>{
-    const user = new UserTest({
-        userName: 'L', 
-        firstName: 'Y'
-    }); 
-    await user.save(); 
-    // const foundUser = await User.findOne({ firstName: 'Y' }); 
-    res.send('<h2> Hello from Express.js server! </h2>'); 
-} ); 
+router.get(
+    '/meetfood',
+    /* authenticateService,  */ async (req, res) => {
+        const user = new UserTest({
+            userName: 'L',
+            firstName: 'Y',
+        });
+        await user.save();
+        // const foundUser = await User.findOne({ firstName: 'Y' });
+        res.send('<h2> Hello from Express.js server! </h2>');
+    },
+);
 
-module.exports = router; 
+module.exports = router;
